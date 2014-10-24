7cd59716ba9785e3e44f7de37a5cbfc2612f8025
gitmana0303 <mr.0303@gmail.com>
2014-10-25 00:36:33 +0900

    add custom __ini__.py
    py2exe で 固める場合、style.qssをlibrary.zipに探しにいってクラッシュするの対策

---
+++ qdarkstyle/__init__.py
@@ -0,0 +1,33 @@
+"""
+Initialise the QDarkStyleSheet module when used with python.
+
+This modules provides a function to transparently load the stylesheets
+with the correct rc file.
+"""
+import os
+
+
+def load_stylesheet(pyside=True):
+    """
+    Loads the stylesheet. Takes care of importing the rc module.
+
+    :param pyside: True to load the pyside rc file, False to load the PyQt rc file
+
+    :return the stylesheet string
+    """
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    # Smart import of the rc file
+    if pyside:
+        import pyside_style_rc
+    else:
+        import pyqt_style_rc
+
+    # Load the stylesheet content
+    ret_val = ""
+    filename = os.path.join(cwd, "style.qss")
+    if not os.path.exists(filename):
+        tmp = cwd.split("\\")
+        filename = os.path.join("\\".join(tmp[0:-2]), "style.qss")
+    with open(os.path.join(__file__, filename)) as stylesheet:
+        ret_val = stylesheet.read()
+    return ret_val
